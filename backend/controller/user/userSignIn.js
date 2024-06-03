ce13f1101c7b58eac82cede5fe190ce5ca19ccda
Amit Prajapati <amitprj796@gmail.com>
2024-06-03 16:30:57 +0530

    same site none

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -32,7 +32,8 @@ async function userSignInController(req,res){
 
         const tokenOption = {
             httpOnly : true,
-            secure : true
+            secure : true,
+            sameSite : 'None'
         }
 
         res.cookie("token",token,tokenOption).status(200).json({
